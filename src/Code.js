368798cfef7a158a174dca23584353ca277de35d
Tim <timothyhollabaugh@gmail.com>
2017-09-19 10:35:20 -0400

    Add google script

---
+++ src/Code.js
@@ -0,0 +1,10 @@
+
+var userSpreadsheet = "1aWH1790Yxu2JNy3OCh-iakoaZQNOalPJmvZi8TuNQYQ";
+
+function doGet(e) {
+  return HtmlService.createHtmlOutputFromFile('index.html');
+}
+
+function getUserInfo(user_id) {
+  
+}
